49cf7804872d67591f0b8e8a6c62421570986950
DmitryG228 <2280905@gmail.com>
2024-02-05 15:38:59 +0300

    d

--- process_connections.py
+++ process_connections.py
@@ -30,6 +30,7 @@ running_tasks = set()
 
 
 
+
 async def get_next_chunk_start(redis_inner_client, audio_name, length,shift):
     diarization = Diarisation(audio_name, redis_inner_client)
     await diarization.get()
@@ -60,24 +61,8 @@ async def diarize(client_id, audio_name, shift, redis_inner_client):
     done = await redis_inner_client.brpop(f'DiarizeReady:{audio_name}')
     diarization = Diarisation(audio_name, redis_inner_client)
     await diarization.get()
-    df = pd.DataFrame(diarization.data)
-    df['len'] = df['end'] - df['start']
-    df = df[df['len']>0.7]
+    return diarization.data
 
-    if len(df)>0:
-        expanded_df = pd.DataFrame(columns=['speaker', 'time'])
-        for index, row in df.iterrows():
-            time_range = np.arange(row['start'] * 100, row['end'] * 100, 1).astype(int)
-
-            temp_df = pd.DataFrame({
-                'speaker': row['speaker'],
-                'time': time_range
-            })
-            expanded_df = pd.concat([expanded_df, temp_df], ignore_index=True)
-        expanded_df['time'] = (expanded_df['time'] / 100.0).astype('float')
-        expanded_df['conv_time'] = (expanded_df['time'] + shift).astype('float')
-
-        return expanded_df.sort_values("time")
     
 
 
@@ -86,25 +71,23 @@ async def transcribe(audio_name, redis_inner_client):
     _,done = await redis_inner_client.brpop(f'TranscribeReady:{audio_name}')
     transcription =  Transcript(audio_name,redis_inner_client)
     await transcription.get()
-    df =  pd.concat([pd.DataFrame(t) for t in transcription.data])
+    return transcription.data
 
 
-    df['start'] = df['start'].astype('float')
-    return df.sort_values("start")
 
 
-async def process_connection(connection_id, redis_stream_client, redis_inner_client, step=20,max_length=600):
+async def process_connection(connection_id, redis_stream_client, redis_inner_client, step=20,max_length=120):
     path = f'/app/testdata/{connection_id}.webm'
 
     redis_stream_client = await get_stream_redis()
-    redis_inner_client = await get_inner_redis()
+    redis_inner_client  = await get_inner_redis()
 
     start = await redis_inner_client.rpop(f'Start:{connection_id}')
     start = float(start) if start else 0
     print('started at ',start)
 
     await writestream2file(connection_id,redis_stream_client)
-    audio_slicer = await AudioSlicer.from_ffmpeg_slice(path,start,max_length)
+    audio_slicer = await AudioSlicer.from_ffmpeg_slice(path,start,start+max_length)
 
     slice_duration = audio_slicer.audio.duration_seconds
     print(slice_duration)
@@ -120,24 +103,12 @@ async def process_connection(connection_id, redis_stream_client, redis_inner_cli
                 diarize(client_id, audio_name, start,redis_inner_client),
                 transcribe(audio_name,redis_inner_client)
             )
-
-        df = pd.merge_asof(transcription_result,diarization_result,left_on = 'start',right_on='time',direction='nearest')
-        df['speaker_change'] = df['speaker'] != df['speaker'].shift()
-        df['silence'] = df['start']-df['end'].shift()
-        df['speaker_change'] = np.where(df['silence']>2,True,df['speaker_change'])
-        df['speaker_change'] = df['speaker_change'].cumsum()
-        df = df.groupby('speaker_change').agg({'speaker': 'first', 'start': 'first', 'end': 'last'}).join(df.groupby('speaker_change').apply(lambda x:''.join(x['word'])).to_frame('text'))
-        df['len'] = df['end'] - df['start']
-        df['speaker'] = df['speaker'].fillna(method='ffill').fillna(method='bfill')
-        df['conv_start'] = df['start']+start
-        df['conv_end'] = df['end']+start
+        
+        await redis_inner_client.lpush(f'Segment:{connection_id}', json.dumps((diarization_result, transcription_result,start)))
 
         start_ = await get_next_chunk_start(redis_inner_client, audio_name, slice_duration,start)
         start = start_ if start_ else start+slice_duration
         await redis_inner_client.lpush(f'Start:{connection_id}', start)
-
-
-        await redis_inner_client.lpush(f'Segment:{connection_id}', json.dumps(df.to_dict(orient='records')))
 
     else:
         await redis_inner_client.lpush(f'Start:{connection_id}', start)
@@ -145,7 +116,7 @@ async def process_connection(connection_id, redis_stream_client, redis_inner_cli
     running_tasks.remove(connection_id)
 
 def task_completed(task, connection_id):
-    running_tasks.remove(connection_id)
+    if connection_id in running_tasks: running_tasks.remove(connection_id)
     print(f"Task for {connection_id} completed")
 
 
@@ -157,11 +128,15 @@ async def check_and_process_connections():
     while True:
         connections = await get_connections('initialFeed_audio', redis_stream_client)
         connection_ids = [c.replace('initialFeed_audio:', '') for c in connections]
+       # connection_ids.append('f7392735-93c1-47d8-83ae-00524eaff239')
         for connection_id in connection_ids:
             if connection_id not in running_tasks:
-                running_tasks.add(connection_id)
-                task = asyncio.create_task(process_connection(connection_id, redis_stream_client, redis_inner_client))
-                task.add_done_callback(lambda t, cid=connection_id: task_completed(t, cid))
+                try:
+                    running_tasks.add(connection_id)
+                    task = asyncio.create_task(process_connection(connection_id, redis_stream_client, redis_inner_client))
+                    task.add_done_callback(lambda t, cid=connection_id: task_completed(t, cid))
+                except:
+                    pass
         #await asyncio.sleep(1)  # Wait for a bit before checking for new connections again
 
 async def main():
