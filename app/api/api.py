4d39b02925353f34083f30bed90a870125c476f4
Andrey Pisankin <andreyy655@gmail.com>
2024-05-13 19:26:42 +0700

    Add tools routes

--- app/api/api.py
+++ app/api/api.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 from app.api.v1.routers.embeddings import router as embeddings_router
 from app.api.v1.routers.health import router as health_router
 from app.api.v1.routers.segments import router as segments_router
+from app.api.v1.routers.tools import router as tools_router
 
 router = APIRouter()
 
@@ -11,5 +12,6 @@ router = APIRouter()
 router.include_router(segments_router, prefix="/v1")
 router.include_router(embeddings_router, prefix="/v1")
 router.include_router(health_router, prefix="/v1")
+router.include_router(tools_router, prefix="/v1")
 
 # you cat place other v1 endpoint or add new v2
---
+++ app/api/v1/routers/tools.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+
+from app.database_redis.connection import get_redis_client
+from app.settings import settings
+
+router = APIRouter(prefix="", tags=["tools"])
+
+
+@router.post("/flush_cache")
+async def flush_cache():
+    client = await get_redis_client(settings.redis_host, settings.redis_port, settings.redis_password)
+    await client.flushdb()
+    return {"message": "cache flushed successfully"}
