0f151afcf801871cbe873bc4df4efd0f9f1c0d62
Andrey Pisankin <andreyy655@gmail.com>
2024-05-22 19:56:20 +0700

    fix get_embeddings func

--- app/database_redis/dals/speaker_dal.py
+++ app/database_redis/dals/speaker_dal.py
@@ -1,6 +1,6 @@
 """Module for basic work with radishes (get value, put value, check connection)."""
 import logging
-from typing import Any
+from typing import Any, List
 
 from app.database_redis import keys
 from app.database_redis.dals.base import BaseDAL
@@ -11,5 +11,8 @@ logger = logging.getLogger(__name__)
 class SpeakerDAL(BaseDAL):
     """Class for basic work with embedding's data in Redis."""
 
-    async def get_embeddings(self, limit: int = 100) -> Any:
-        return await self.rpop_many(key=keys.SPEAKER_EMBEDDINGS, limit=limit)
+    async def get_embeddings(self, limit: int = 100) -> List[Any]:
+        embeddings = await self.rpop_many(key=keys.SPEAKER_EMBEDDINGS, limit=limit)
+        if embeddings:
+            return embeddings
+        return []
