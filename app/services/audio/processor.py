714872d8a4a8d0bacb919307ce0063b8b25e7d0b
Andrey Pisankin <andreyy655@gmail.com>
2024-08-12 19:08:57 +0700

    0 Return transcriber_step

--- app/services/audio/processor.py
+++ app/services/audio/processor.py
@@ -27,7 +27,12 @@ class Processor:
             logger.info(f"Found {connection.amount} chink(s)")
             await self._process_connection_task(connection.connection_id)  # ToDo: asyncio task
 
-    async def _process_connection_task(self, connection_id, diarizer_step: int = 30, transcriber_step: int = 5) -> None:
+    async def _process_connection_task(
+        self,
+        connection_id,
+        diarizer_step: int = 30,
+        transcriber_step: int = 10,
+    ) -> None:
         redis_client = await get_redis_client(settings.redis_host, settings.redis_port, settings.redis_password)
         meeting_id, segment_start_timestamp, segment_end_timestamp, user_id = await self.writestream2file(connection_id)
 
